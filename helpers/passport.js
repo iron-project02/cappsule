e3ca13c5d0c3bccac4c3a786ca4021488eff3fce
Mauricio Gonzalez Robles <mauricio.gzrs@gmail.com>
2018-10-25 14:59:45 -0500

    Passport Strategy helper

---
+++ helpers/passport.js
@@ -0,0 +1,10 @@
+const passport = require(`passport`),
+      User     = require(`../models/User`);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+
+passport.deserializeUser(User.deserializeUser());
+
+module.exports = passport;
