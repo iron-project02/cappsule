65f4931a82ecce29ff712024a48cf9eef78a81f8
Mauricio Gonzalez Robles <mauricio.gzrs@gmail.com>
2018-10-25 17:54:16 -0500

    Auth Post routes

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -9,17 +9,43 @@ authSites.get(`/auth/register`, (req,res) => {
     title:    `Register`,
     css:      `auth`,
     register: true
-  }
+  };
   res.render(`auth/auth`, {data});
+});
+
+authSites.post(`/auth/register`, (req,res) => {
+  let data = {
+    title:    `Register`,
+    css:      `auth`,
+    register: true
+  };
+  if (req.body.password !== req.body[`confirm-password`]) return res.render(`auth/auth`, {data, errorMessage: `Passwords don't coincide`});
+  const {username, email, name, password} = req.body;
+  User.register({username, email, name}, password)
+      .then( user => {
+        // const options = {
+        //   email:   user.email,
+        //   subject: `Confirma tu correo`,
+        //   message: `Confirm to get cookies`
+        // };
+        // mail.send(options);
+        res.redirect(`/auth/login`);
+      })
+      .catch( err => res.status(500).render(`register`, {err, errorMessage: `There was an error, please try again`}) );
 });
 
 authSites.get(`/auth/login`, (req,res) => {
   let data = {
     title: `Login`,
     css:   `auth`,
-  }
-  res.render(`auth/auth`, {data, errorMessage: `ok`});
+  };
+  res.render(`auth/auth`, {data});
 });
+
+authSites.post(`/auth/login`, passport.authenticate(`local`, {
+  successRedirect: `/`,
+  failureRedirect: `/auth/login`
+}));
 
 authSites.get(`/auth/logout`, (req,res) => {
   req.logout();
