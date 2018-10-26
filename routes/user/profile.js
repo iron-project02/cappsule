ef3ff5ed721faec7a4afef0609118352e5e61267
Mauricio Gonzalez Robles <mauricio.gzrs@gmail.com>
2018-10-26 17:31:17 -0500

    User CRUD - GET & POST
    Profile view
    Edit User form
    Edit User form styles

--- routes/user/profile.js
+++ routes/user/profile.js
@@ -1,6 +1,7 @@
 const express   = require(`express`),
       userSites = express.Router(),
       check     = require(`../../helpers/checker`),
+      multer    = require(`../../helpers/multer`),
       User      = require(`../../models/User`);
 
 userSites.get(`/user/:id`, check.isLogged, check.isUser, (req,res) => {
@@ -9,9 +10,30 @@ userSites.get(`/user/:id`, check.isLogged, check.isUser, (req,res) => {
         let data = {
           title: user.name,
           css:   `profile`
-        }
+        };
         res.render(`private/profile`, {data, user});
       });
+});
+
+userSites.get(`/user/:id/edit`, check.isLogged, check.isUser, (req,res) => {
+  const {user} = req;
+  let   data   = {
+          title: user.name,
+          css:   `user-edit`
+        };
+  res.render(`private/userEdit`, {data, user})
+});
+
+userSites.post(`/user/:id/edit`, check.isLogged, check.isUser, multer.single(`profile_pic`), (req,res) => {
+  const {user} = req;
+  if (req.file !== undefined) req.body.profile_pic = req.file.url;
+  User.findByIdAndUpdate(req.params.id, {$set: req.body})
+      .then( () => res.redirect(`/user/${user._id}`) );
+});
+
+userSites.get(`/user/:id/delete`, check.isLogged, check.isUser, (req,res) => {
+  User.findByIdAndDelete(req.user._id)
+      .then( () => res.redirect(`/`) );
 });
 
 
