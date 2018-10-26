9089815797f316243251ce30fd159be48d8baad6
Mauricio Gonzalez Robles <mauricio.gzrs@gmail.com>
2018-10-26 13:41:22 -0500

    Update checker.js
    checkUser added

--- helpers/checker.js
+++ helpers/checker.js
@@ -1,4 +1,15 @@
+const User = require(`../models/User`);
+
 exports.isLogged = (req,res,next) => {
   if (req.isAuthenticated()) return next();
   res.redirect(`/auth/login`);
-}
+};
+
+exports.isUser = async function checkUser(req,res,next) {
+  const user = await User.findById(req.params.id);
+  if (user._id.toString() === req.user._id.toString()) {
+    req.user = user;
+    return next();
+  }
+  res.redirect(`/auth/login`);
+};
