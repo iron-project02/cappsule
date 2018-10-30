94371461bbe50c6611ca556b33795184d7e7d4d7
Mauricio González Robles <mauricio.gzrs@gmail.com>
2018-10-29 22:44:54 -0600

    Admin updated
    Admin:
    view
    user crud
    axios
    css

--- public/js/admin.js
+++ public/js/admin.js
@@ -7,24 +7,129 @@ window.onload = () => {
   console.log(`Running admin.js`);
 
   addEventListener(`submit`, onSubmit);
+  addEventListener(`click`, onDelete);
 
   function onSubmit(e) {
     e.preventDefault();
-    if (e.target.id === `search-user`) {
-      axios .get(`${window.location.href}/search?${e.target[0].name}=${e.target[0].value}`)
-            .then(search => {
-              console.log(search);
-              const container = document.querySelector(`#users-crud .search-results-wrapper`),
-                    list      = document.createElement(`ul`);
-              container.innerHTML = ``;
-              list.className = `search-results`;
-              container.appendChild(list);
-              search.data.forEach(user => {
-                let li = document.createElement(`li`);
-                li.innerHTML = `<a href="${window.location.href}/edit/${user._id}">${user.name}</a>`;
-                list.appendChild(li)
-              });
-            });
-    }
+    getUsers(e);
+    updateUser(e);
+  }
+  function onDelete(e) {
+    deleteUser(e);
   }
 };
+
+function getUsers(e) {
+  if (e.target.id === `search-user`) {
+    axios .get(`${window.location.href}/search?${e.target[0].name}=${e.target[0].value}`)
+          .then(search => {
+            const container = document.getElementById(`user-results`);
+            container.innerHTML = ``;
+            search.data.forEach(user => {
+              container.insertAdjacentHTML(`beforeend`, `
+              <div>
+                <div class="result-card uk-card uk-card-default uk-card-small">
+                  <div class="uk-card-header">
+                    <h4 class="uk-card-title">${user.name}</h4>
+                  </div>
+                  <form action="${window.location.href}" method="POST" enctype="application/x-www-form-urlencoded" id="update-user" class="uk-card-body">
+                    <div class="result-field">
+                      <label>Role: </label>
+                      <select name="role">
+                        <option value="${user.role}" selected disabled>${user.role}</option>
+                        <option value="admin">Administrator</option>
+                        <option value="user">User</option>
+                      </select>
+                    </div>
+                    <div class="result-field">
+                      <label>Phone: </label>
+                      <input name="username" value="${user.username}" required>
+                    </div>
+                    <div class="result-field">
+                      <label>Email: </label>
+                      <input name="email" value="${user.email}" required>
+                    </div>
+                    <div class="result-field">
+                      <label>Name: </label>
+                      <input name="name" value="${user.name}" required>
+                    </div>
+                    <div class="result-field">
+                      <label>Age: </label>
+                      <input name="age" value="${user.age}" type="number">
+                    </div>
+                    <div class="result-field">
+                      <label>Gender: </label>
+                      <select name="gender">
+                        <option value="${user.gender}" selected disabled>${user.gender}</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                      </select>
+                    </div>
+                    <div class="result-field">
+                      <label>Address: </label>
+                      <input name="address" value="${user.address}">
+                    </div>
+                    <input type="hidden" name="userID" value="${user._id}">
+                    <div class="submit-form">
+                      <button type="submit">Update</button>
+                      <button data-event="delete" data-click="${window.location.href}/delete/${user._id}" type="button">Delete</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              `);
+            });
+          });
+  }
+}
+
+function updateUser(e) {
+  if (e.target.id === `update-user`) {
+    let inputs = e.target,
+        obj    = {};
+    for (let i = 0; i < inputs.length - 1; i++) {
+      obj[inputs[i].name] = inputs[i].value; // To place the keys and values in obj
+    }
+    axios .patch(`${window.location.href}/update?${e.target[1].name}=${e.target[1].value}`, obj)
+          .then(result => {
+            let form   = e.target,
+                user   = result.data;
+            form[0].value     = user.role;
+            form[1].value     = user.username;
+            form[2].value     = user.email;
+            form[3].value     = user.name;
+            form[4].value     = user.age;
+            form[5].value     = user.gender;
+            form[6].value     = user.address;
+            form[8].classList = `updated`;
+            form[8].innerText = `Updated`;
+            setTimeout(()=> {
+              form[8].classList = ``;
+              form[8].innerText = `Update`;
+            }, 1500)
+          })
+          .catch(err => {
+            let form = e.target;
+            form[8].classList = `error`;
+            form[8].innerText = `Error`;
+          });
+  }
+}
+
+function deleteUser(e) {
+  if (e.target.dataset.event === `delete`) {
+    let btn   = e.target,
+        cards = e.target.parentNode.parentNode.parentNode.parentNode.parentNode,
+        card  = e.target.parentNode.parentNode.parentNode.parentNode;
+
+    
+    axios .delete(e.target.dataset.click)
+          .then(() => {
+            cards.removeChild(card);
+          })
+          .catch(err => {
+            btn.classList = `error`;
+            btn.innerText = `Error`;
+          });
+  }
+}
--- routes/user/admin.js
+++ routes/user/admin.js
@@ -1,9 +1,10 @@
 const express    = require(`express`)
       adminSites = express.Router(),
+      check      = require(`../../helpers/checker`),
       User       = require(`../../models/User`),
       Offer      = require(`../../models/Offer`);
 
-adminSites.get(`/admin/:id/search`, (req,res) => {
+adminSites.get(`/admin/:id/search`, check.isLogged, check.isAdmin, (req,res) => {
   if (req.query.product !== undefined) {
     return User.find({email: `mao@mao.com`}).then(users => res.json(users));
   }
@@ -11,6 +12,16 @@ adminSites.get(`/admin/:id/search`, (req,res) => {
     let query = new RegExp(`.*${req.query.email}.*`);
     return User.find({email: { $in: [query] } }).then(search => res.json(search));
   }
+});
+
+adminSites.patch(`/admin/:id/update`, check.isLogged, check.isAdmin, (req,res) => {
+  if (req.query.username !== undefined) {
+    return User.findByIdAndUpdate(req.body.userID, {$set: req.body}, {new: true}).then(user => res.json(user));
+  }
+});
+
+adminSites.delete(`/admin/:id/delete/:user`, check.isLogged, check.isAdmin, (req,res) => {
+  return User.findByIdAndDelete(req.params.user).then(user => res.json(user));
 });
 
 module.exports = adminSites;
