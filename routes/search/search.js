a1a52242c8c82d37eb9601f1889a9a05ce219062
Mauricio González Robles <mauricio.gzrs@gmail.com>
2018-10-31 14:47:23 -0600

    Search Route

--- routes/search/search.js
+++ routes/search/search.js
@@ -1,4 +1,18 @@
-const express   = require('express'),
-      searSites = express.Router();
+const express     = require('express'),
+      searchSites = express.Router(),
+      check       = require(`../../helpers/checker`),
+      Product     = require(`../../models/Product`);
 
-module.exports = searSites;
+searchSites.get(`/search`, check.isLogged, (req,res) => {
+  let query = new RegExp(`.*${req.query.name}.*`);
+    Product .find({name: { $in: [query] } })
+            .then(products => {
+              let data = {
+                title: `"${req.query.name}" - Results`,
+                css:   `search`
+              };
+              res.render(`private/search`, {data, products});
+            });
+});
+
+module.exports = searchSites;
