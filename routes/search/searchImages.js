5427b2193832cd8247c48d38bc9d64689aae2cc1
Vermont <vermont.ironhack@gmail.com>
2018-11-05 23:03:14 -0600

    Changing the approach

--- routes/search/searchImages.js
+++ routes/search/searchImages.js
@@ -9,27 +9,44 @@ searchSites.get(`/search/image`, check.isLogged, (req, res) =>{
   
   res.render('private/searchImages.hbs');
 
-})
+});
+
+searchSites.post(`/image`, check.isLogged, (req, res) =>{
+  
+  res.render('private/searchImages.hbs');
+  console.log(req.body.imageString)
+  
+});
 
 searchSites.post(`/search/image`, check.isLogged, (req, res) =>{
-  
-  console.log('Front Data ======>', req.body.imageString.length)
-
-  //res.json(req.body.imageString.length)
+  //res.render('private/search.hbs');
+  //console.log('Front Data ======>', req.body.imageString.length)
 
   //hacer la peticion a Google
-
+  
   getImageData(req.body.imageString)
     .then(imageData => {
+
+
+      // Aqui hacer la peticion a la busqueda de las farmacias y luego enviar
+      // la informacion a la vista de los resultados de la busqueda
+
       console.log('Image Data from Google ======> ', imageData.data.responses)
-      let medicine = imageData.data.responses;
+      let medicine = imageData.data.responses[0].textAnnotations[0].description;
+      let medicine2 = imageData.data.responses[0].fullTextAnnotation.text;
+      console.log('Text =====> ', medicine )
+      console.log('Text =====> ', medicine2 )
       //res.redirect('/');
-      //res.render('private/search', {medicine})
-      res.json(medicine);
+      let variable = {
+        name: 'Hola Mundo'
+      }
+      res.render('private/search', {variable, medicine, medicine2})
+      //res.json(medicine);
     })
     .catch(err => {
       console.log('Error google request =====>', err)
     })
+    
 });
 
 const getImageData = async (data) => {
