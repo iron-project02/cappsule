31c8f6fafd1003d140a679389c0d7e3cb8ec2caf
Vermont <vermont.ironhack@gmail.com>
2018-11-07 13:53:11 -0600

    Search by image implemented

--- routes/search/searchImages.js
+++ routes/search/searchImages.js
@@ -7,26 +7,71 @@ const express     = require('express'),
 
 
 searchSites.get(`/search/image`, check.isLogged, (req, res) =>{
+
+  const {user} = req;
   
-  res.render('private/searchImages.hbs');
+  res.render('private/searchImages.hbs', {user});
 
 })
 
 searchSites.post(`/search/image`, check.isLogged, (req, res) =>{
   
-  console.log('Front Data ======>', req.body.imageString.length)
-
-  //res.json(req.body.imageString.length)
-
-  //hacer la peticion a Google
+  const {user} = req;
 
   auxFunc.getImageData(req.body.imageString)
     .then(imageData => {
+
       console.log('Image Data from Google ======> ', imageData.data.responses)
-      let medicine = imageData.data.responses;
-      //res.redirect('/');
-      //res.render('private/search', {medicine})
-      res.json(medicine);
+      let medicine = imageData.data.responses[0].textAnnotations[1].description;
+      let medicine2 = imageData.data.responses[0].textAnnotations[2].description;
+      //let medicine2 = imageData.data.responses[0].fullTextAnnotation.text;
+      let variable = {
+        description: imageData.data.responses[0].fullTextAnnotation.text
+      }
+      let data = {
+        title: "Search by Image",
+        css:   `search`,
+        js:    `search`
+      }
+            
+      //////
+
+      axios .all([auxFunc.getSanPablo(medicine), auxFunc.getAhorro(medicine)])
+        .then(axios.spread((FSP, FA) => {
+          let obj  = {},
+              html = require(`../../helpers/searchHTML`);
+
+          // Data treatment
+          obj.SanPablo = auxFunc.sanPabloResults(FSP.data);
+          obj.Ahorro   = auxFunc.delAhorroResults(FA.data);
+
+          obj.notfound = html.noResults();
+          obj.html     = html.productCard();
+
+          let products = obj.SanPablo.map(item => item);
+
+          obj.Ahorro.forEach(item => products.push(item));
+
+          (async function sortProducts(all) {
+            all.sort((a,b) => {
+              let priceA = parseFloat(a.price),
+                  priceB = parseFloat(b.price);
+              return priceA - priceB;
+            });
+            await console.log(allProducts);
+          })(products);
+
+          res.render('private/search', {data, user, variable, products, medicine, medicine2})
+
+          //res.json(obj);
+        }));
+
+
+
+      //////
+      
+      
+      //res.json(medicine);
     })
     .catch(err => {
       console.log('Error google request =====>', err)
