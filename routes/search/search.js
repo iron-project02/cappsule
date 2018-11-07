dd0c95faa3d6a16fd0dc048adae8f693e70daa6a
Mauricio González Robles <mauricio.gzrs@gmail.com>
2018-11-07 12:13:10 -0600

    Get && Render product results

--- routes/search/search.js
+++ routes/search/search.js
@@ -2,56 +2,34 @@ const express     = require('express'),
       searchSites = express.Router(),
       check       = require(`../../helpers/checker`),
       auxFunc     = require(`../../helpers/auxFunctions`),
-      axios       = require(`axios`),
-      Product     = require(`../../models/Product`);
+      axios       = require(`axios`);
 
 searchSites.get(`/search`, check.isLogged, (req,res) => {
-  let query = new RegExp(`.*${req.query.name}.*`);
-    Product .find({$or : [{name: {$regex: query, $options: `i`}}, {ingredient: {$regex: query, $options: `i`}}]})
-            .sort({price: 1})
-            .then(products => {
-              const {user} = req;
-              let data = {
-                title: `"${req.query.name}" - Results`,
-                css:   `search`,
-                js:    `search`
-              };
-              res.render(`private/search`, {data, user, products});
-            });
-});
-
-// searchSites.get(`/prod`, check.isLogged, (req,res) => {
-//   let query = new RegExp(`.*${req.query.name}.*`);
-//   Product .find({$or : [{name: {$regex: query, $options: `i`}}, {ingredient: {$regex: query, $options: `i`}}]})
-//           .sort({price: 1})
-//           .then(search => {
-//             let html = require(`../../helpers/searchHTML`);
-//             search.push(html.noResults());
-//             search.push(html.productCard());
-//             res.json(search)
-//           });
-// })
-
-searchSites.get(`/prod`, check.isLogged, (req,res) => {
   axios .all([auxFunc.getSanPablo(req.query.name), auxFunc.getAhorro(req.query.name)])
         .then(axios.spread((FSP, FA) => {
-          let obj  = {},
-              html = require(`../../helpers/searchHTML`);
+          let data = {
+                title: `"${req.query.name}" - Results`,
+                css:   `search`
+              },
+              obj  = {};
 
           // Data treatment
           obj.SanPablo = auxFunc.sanPabloResults(FSP.data);
           obj.Ahorro   = auxFunc.delAhorroResults(FA.data);
 
-          obj.notfound = html.noResults();
-          obj.html     = html.productCard();
-
-          res.json(obj);
+          let products = obj.SanPablo.map(item => item);
+          obj.Ahorro.forEach(item => products.push(item));
+          
+          (async function sortProducts(all) {
+            all.sort((a,b) => {
+              let priceA = parseFloat(a.price),
+                  priceB = parseFloat(b.price);
+              return priceA - priceB;
+            });
+            await res.render(`private/search`, {data, products});
+          })(products);
         }));
 });
 
-searchSites.post(`/prod`, check.isLogged, (req,res) => {
-  Product.create()
-  console.log(req.body);
-});
 
 module.exports = searchSites;
