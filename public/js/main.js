b6403701ffc3810b44648d7e4b2940198e77c76e
Mauricio González Robles <mauricio.gzrs@gmail.com>
2018-11-06 22:28:11 -0600

    Back to top on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,13 @@ window.onload = () => {
   console.log(`Running!`);
 
   let searchDiv = document.querySelector(`[href='#search-modal']`);
-  if(searchDiv) searchDiv.focus();
+  if (searchDiv) searchDiv.focus();
+};
+
+window.onscroll = e => {
+  const btt = document.querySelector(`.back-to-top`);
+  if (btt) {
+    if (e.pageYOffset > 200) btt.classList.add(`show-btt`);
+    if (e.pageYOffset < 200) btt.classList.remove(`show-btt`);
+  }
 };
